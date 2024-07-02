60988890aadcfae3159271f858c534f651801bb3
BIT\BITCAMP <ponde119@outlook.com>
2024-07-02 21:17:07 +0900

    24.07.02 이준섭 다크모드시에 뷰 다르게 보이는 거 수정

--- project-tmp/js/darkmode.js
+++ project-tmp/js/darkmode.js
@@ -11,8 +11,19 @@ function applyDarkMode(isDarkMode) {
     if (isDarkMode) {
         $('body').addClass('dark-mode');
         $('#darkModeToggle').text("").append(`<i class="bi bi-moon-fill"></i><div>화면 모드</div>`)
+        applyDisplayDarkMode(isDarkMode);
     } else {
         $('body').removeClass('dark-mode');
         $('#darkModeToggle').text("").append(`<i class="bi bi-brightness-high"></i><div>화면 모드</div>`);
+    }
+}
+
+function applyDisplayDarkMode(isDarkMode){
+    if(isDarkMode){
+        $('#darModeToggle').parent.childeren.hover(function(){
+            $(this).childeren().css("background", "#dddddd");
+        }, function(){
+            $(this).childeren().css("background", "#121212");
+        })
     }
 }
