419a1754978dddd3e0998239eb5ca99c3bb79133
BIT\BITCAMP <ponde119@outlook.com>
2024-07-03 17:03:41 +0900

    main.html 통합

--- project-tmp/js/darkmode.js
+++ project-tmp/js/darkmode.js
@@ -10,20 +10,9 @@ function loadDarkModeState() {
 function applyDarkMode(isDarkMode) {
     if (isDarkMode) {
         $('body').addClass('dark-mode');
-        $('#darkModeToggle').text("").append(`<i class="bi bi-moon-fill"></i><div>화면 모드</div>`)
-        applyDisplayDarkMode(isDarkMode);
+        $('#darkModeToggle').text("").append(`<i class="bi bi-moon-fill"></i><div>화면 모드</div>`);
     } else {
         $('body').removeClass('dark-mode');
         $('#darkModeToggle').text("").append(`<i class="bi bi-brightness-high"></i><div>화면 모드</div>`);
-    }
-}
-
-function applyDisplayDarkMode(isDarkMode){
-    if(isDarkMode){
-        $('#darModeToggle').parent.childeren.hover(function(){
-            $(this).childeren().addClass(".sidebar-item-dark-mode");
-        }, function(){
-            $(this).childeren().removeClass(".sidebar-item-dark-mode");
-        })
     }
 }
