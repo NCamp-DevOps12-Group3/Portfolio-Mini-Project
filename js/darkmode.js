4d8b3607ca961c05b7942a25086f3dd5a13b53e2
WATASHIJXXNSUKA\shoon <jxxn2538@gmail.com>
2024-07-04 10:39:57 +0900

    Modal css 분리

---
+++ js/darkmode.js
@@ -0,0 +1,27 @@
+// 다크모드 관련 함수
+function saveDarkModeState(isDarkMode) {
+    localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+}
+
+function loadDarkModeState() {
+    return localStorage.getItem('darkMode') === 'enabled';
+}
+
+function applyDarkMode(isDarkMode) {
+    if (isDarkMode) {
+        $('body').addClass('dark-mode');
+        $('#darkModeToggle').text("").append(`<i class="bi bi-moon-fill"></i><div>화면 모드</div>`);
+    } else {
+        $('body').removeClass('dark-mode');
+        $('#darkModeToggle').text("").append(`<i class="bi bi-brightness-high"></i><div>화면 모드</div>`);
+    }
+}
+function applyDisplayDarkMode(isDarkMode){
+    if(isDarkMode){
+        $('#darkModeToggle').parent().children().hover(function(){
+            $(this).children().addClass("sidebar-item-dark-mode");
+        }, function(){
+            $(this).children().removeClass("sidebar-item-dark-mode");
+        })
+    }
+}
